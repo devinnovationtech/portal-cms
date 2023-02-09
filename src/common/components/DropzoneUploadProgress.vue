53643339e0f0743a02b5bc8b9a3783f610b12c3a
Ida Bagus Wedagama <33661143+Ibwedagama@users.noreply.github.com>
2023-02-09 13:45:14 +0700

    feat: add popup banner image preview (#240)

--- src/common/components/DropzoneUploadProgress.vue
+++ src/common/components/DropzoneUploadProgress.vue
@@ -1,123 +1,149 @@
 <template>
-  <div
-    :class="{
-      'min-h-[110px] w-full rounded-md px-5 py-4 border border-blue-600': true,
-      'border-red-600': status === 'ERROR',
-    }"
-  >
-    <div class="grid grid-cols-[auto,1fr,auto] gap-x-6">
-      <div class="relative w-[86px] h-[75px] overflow-hidden rounded-md">
-        <!-- Display image using url if has an url (for edit mode) -->
-        <img
-          v-if="status === 'HASDEFAULT'"
-          :src="imageUrl"
-          class="w-full h-full object-cover"
-        >
-        <img
-          v-else
-          :src="image"
-          class="w-full h-full object-cover"
-        >
-        <transition name="fade">
-          <div
-            v-if="status === 'UPLOADING'"
-            class="absolute bottom-0 w-full h-0 bg-[#1975CA] opacity-60 transition-all ease-out"
-            :style="{ height: `${progress}%` }"
-          />
-        </transition>
-      </div>
-      <div class="flex flex-col justify-between">
-        <div class="flex mb-3">
-          <p class="font-lato font-bold text-[15px] leading-6 line-clamp-1 break-all">
-            {{ fileName }}
-          </p>
-          <span
-            v-show="status === 'SUCCESS'"
-            class="ml-4"
+  <section>
+    <div
+      :class="{
+        'min-h-[110px] w-full rounded-md px-5 py-4 border border-blue-600': true,
+        'border-red-600': status === 'ERROR',
+      }"
+    >
+      <div class="grid grid-cols-[auto,1fr,auto] gap-x-6">
+        <div class="relative w-[86px] h-[75px] overflow-hidden rounded-md">
+          <!-- Display image using url if has an url (for edit mode) -->
+          <img
+            v-if="status === 'HASDEFAULT'"
+            :src="imageUrl"
+            class="w-full h-full object-cover"
+          >
+          <img
+            v-else
+            :src="image"
+            class="w-full h-full object-cover"
+          >
+          <transition name="fade">
+            <div
+              v-if="status === 'UPLOADING'"
+              class="absolute bottom-0 w-full h-0 bg-[#1975CA] opacity-60 transition-all ease-out"
+              :style="{ height: `${progress}%` }"
+            />
+          </transition>
+          <!-- Preview Button -->
+          <transition name="fade">
+            <button
+              v-if="status === 'SUCCESS' || status === 'HASDEFAULT'"
+              type="button"
+              class="absolute inset-0 w-full h-full flex items-center justify-center hover:bg-black hover:opacity-60 transition-all ease-in group"
+              @click="toggleImagePreview"
+            >
+              <div class="flex flex-col opacity-0 group-hover:opacity-100 items-center justify-center">
+                <EyeIcon class="mb-1" />
+                <small class="text-white text-[8px]">Klik untuk melihat <br> gambar</small>
+              </div>
+            </button>
+          </transition>
+        </div>
+        <div class="flex flex-col justify-between">
+          <div class="flex mb-3">
+            <p class="font-lato font-bold text-[15px] leading-6 line-clamp-1 break-all">
+              {{ fileName }}
+            </p>
+            <span
+              v-show="status === 'SUCCESS'"
+              class="ml-4"
+            >
+              <JdsIcon
+                name="check-mark"
+                size="16px"
+                class="h-4 text-green-900"
+              />
+            </span>
+          </div>
+          <div>
+            <!-- UPLOAD PROGRESS PERCENTAGE -->
+            <p
+              v-show="status === 'UPLOADING'"
+              class="text-sm"
+            >
+              {{ progress }}%
+            </p>
+            <!-- SUCCESS -->
+            <p
+              v-show="status === 'SUCCESS'"
+              class="col-span-3 w-full font-lato text-sm text-gray-800"
+            >
+              Gambar berhasil diupload
+            </p>
+            <!-- ERROR -->
+            <p
+              v-show="status === 'ERROR'"
+              class="col-span-3 w-full font-lato text-sm text-gray-800"
+            >
+              Upload gagal, coba periksa koneksi anda kembali
+            </p>
+            <!-- SIZE (for edit purposes) -->
+            <p
+              v-show="fileSize && status === 'HASDEFAULT'"
+              class="col-span-3 w-full font-lato text-sm text-gray-800"
+            >
+              {{ fileSize }}
+            </p>
+          </div>
+        </div>
+        <div class="self-center">
+          <!-- Delete Button -->
+          <button
+            v-if="status === 'SUCCESS' || status === 'HASDEFAULT'"
+            type="button"
+            class="w-7 h-7 flex items-center justify-center bg-red-50 rounded-full"
+            @click="$emit('delete')"
           >
             <JdsIcon
-              name="check-mark"
+              name="trash"
               size="16px"
-              class="h-4 text-green-900"
+              class="h-4 text-red-600"
             />
-          </span>
+          </button>
+          <!-- Retry Button -->
+          <button
+            v-if="status === 'ERROR'"
+            type="button"
+            class="w-7 h-7 flex items-center justify-center bg-blue-gray-50 rounded-full"
+            @click="$emit('retry')"
+          >
+            <RetryIcon />
+          </button>
         </div>
-        <div>
-          <!-- UPLOAD PROGRESS PERCENTAGE -->
-          <p
-            v-show="status === 'UPLOADING'"
-            class="text-sm"
-          >
-            {{ progress }}%
-          </p>
-          <!-- SUCCESS -->
-          <p
-            v-show="status === 'SUCCESS'"
-            class="col-span-3 w-full font-lato text-sm text-gray-800"
-          >
-            Gambar berhasil diupload
-          </p>
-          <!-- ERROR -->
-          <p
-            v-show="status === 'ERROR'"
-            class="col-span-3 w-full font-lato text-sm text-gray-800"
-          >
-            Upload gagal, coba periksa koneksi anda kembali
-          </p>
-          <!-- SIZE (for edit purposes) -->
-          <p
-            v-show="fileSize && status === 'HASDEFAULT'"
-            class="col-span-3 w-full font-lato text-sm text-gray-800"
-          >
-            {{ fileSize }}
-          </p>
-        </div>
-      </div>
-      <div class="self-center">
-        <!-- Delete Button -->
-        <button
-          v-if="status === 'SUCCESS' || status === 'HASDEFAULT'"
-          type="button"
-          class="w-7 h-7 flex items-center justify-center bg-red-50 rounded-full"
-          @click="$emit('delete')"
-        >
-          <JdsIcon
-            name="trash"
-            size="16px"
-            class="h-4 text-red-600"
-          />
-        </button>
-        <!-- Retry Button -->
-        <button
-          v-if="status === 'ERROR'"
-          type="button"
-          class="w-7 h-7 flex items-center justify-center bg-blue-gray-50 rounded-full"
-          @click="$emit('retry')"
-        >
-          <RetryIcon />
-        </button>
-      </div>
-      <!-- UPLOAD PROGRESS BAR -->
-      <div
-        v-show="status === 'UPLOADING'"
-        class="col-span-3 w-full h-2 bg-gray-100 rounded-lg mt-4"
-      >
+        <!-- UPLOAD PROGRESS BAR -->
         <div
-          class="bg-blue-800 h-2 rounded-lg transition-all ease-in"
-          :style="{ width: `${progress}%` }"
-        />
+          v-show="status === 'UPLOADING'"
+          class="col-span-3 w-full h-2 bg-gray-100 rounded-lg mt-4"
+        >
+          <div
+            class="bg-blue-800 h-2 rounded-lg transition-all ease-in"
+            :style="{ width: `${progress}%` }"
+          />
+        </div>
       </div>
     </div>
-  </div>
+
+    <ImagePreview
+      :source="imagePreviewSource"
+      :open="isImagePreviewOpen"
+      @close="toggleImagePreview"
+    />
+  </section>
 </template>
 
 <script>
 import RetryIcon from '@/assets/icons/retry.svg?inline';
+import EyeIcon from '@/assets/icons/eye.svg?inline';
+import ImagePreview from '@/common/components/ImagePreview.vue';
 
 export default {
   name: 'DropzoneUploadProgress',
   components: {
     RetryIcon,
+    EyeIcon,
+    ImagePreview,
   },
   props: {
     progress: {
@@ -148,6 +174,7 @@ export default {
   data() {
     return {
       image: null,
+      isImagePreviewOpen: false,
     };
   },
   computed: {
@@ -162,6 +189,13 @@ export default {
       const sizeToMB = this.imageSize / 1000000;
       return `${sizeToMB.toFixed(2)} MB`;
     },
+    imagePreviewSource() {
+      if (this.status === 'HASDEFAULT') {
+        return this.imageUrl;
+      }
+
+      return this.image;
+    },
   },
   watch: {
     file: {
@@ -174,6 +208,11 @@ export default {
       },
       deep: true,
       immediate: true,
+    },
+  },
+  methods: {
+    toggleImagePreview() {
+      this.isImagePreviewOpen = !this.isImagePreviewOpen;
     },
   },
 };
---
+++ src/common/components/ImagePreview.vue
@@ -0,0 +1,31 @@
+<template>
+  <BaseModal
+    :open="open"
+    @close="$emit('close')"
+  >
+    <img
+      :src="source"
+      alt=""
+      class="w-full h-full object-contain"
+    >
+  </BaseModal>
+</template>
+
+<script>
+import BaseModal from './BaseModal.vue';
+
+export default {
+  name: 'ImagePreview',
+  components: { BaseModal },
+  props: {
+    open: {
+      type: Boolean,
+      default: false,
+    },
+    source: {
+      type: String,
+      default: '',
+    },
+  },
+};
+</script>
