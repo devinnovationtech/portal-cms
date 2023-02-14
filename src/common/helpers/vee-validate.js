026b9a65ec87e99bac34f33547e199af0a27d07d
Ida Bagus Wedagama <33661143+Ibwedagama@users.noreply.github.com>
2023-02-14 09:08:41 +0700

    fix: regex url validation (#247)

--- src/common/helpers/vee-validate.js
+++ src/common/helpers/vee-validate.js
@@ -20,7 +20,7 @@ extend('size', {
 extend('url', {
   validate(value) {
     // eslint-disable-next-line no-useless-escape
-    const urlPattern = new RegExp(/^(https?:\/\/.)[-a-zA-Z0-9@:%._\+~#=]{2,256}\.[a-z]{2,6}\b([-a-zA-Z0-9@:%_\+.~#?&//=]*)$/g);
+    const urlPattern = new RegExp(/[(http(s)?):\/\/(www\.)?a-zA-Z0-9@:%._\+~#=]{2,256}\.[a-z]{2,6}\b([-a-zA-Z0-9@:%_\+.~#?&//=]*)/ig);
     return urlPattern.test(value);
   },
   message: 'Link yang anda masukkan salah!',
