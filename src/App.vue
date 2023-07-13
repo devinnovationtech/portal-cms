16993ca2e20947991e7443cc5781153245863a4b
Ida Bagus Wedagama <33661143+Ibwedagama@users.noreply.github.com>
2023-07-13 14:48:04 +0700

    feat: add new relic (#440)
    * feat (new-relic): add new relic browser monitoring
    
    * feat: add new relic error logging and monitoring
    
    * chore (env-example): remove sentry env
    
    * refactor (new-relic): remove async property on script

--- src/App.vue
+++ src/App.vue
@@ -27,6 +27,17 @@ export default {
       meta: [
         { name: 'robots', content: 'noindex' }, // prevent search engine to index this site
       ],
+      script: [
+        // Initiate New Relic Browser Monitoring Script
+        {
+          src: './new-relic-browser-script.js',
+          type: 'text/javascript',
+          defer: true,
+          'data-account-id': process.env.VUE_APP_ACCOUNT_ID_NEW_RELIC,
+          'data-browser-license-key': process.env.VUE_APP_LICENSE_KEY_NEW_RELIC_ACCOUNT,
+          'data-application-id': process.env.VUE_APP_APPLICATION_ID_NEW_RELIC,
+        },
+      ],
     };
   },
 };
