38c2155a74a6dd5c1a79cd03479ab65d76300187
naufalihsank <56071566+naufalihsank@users.noreply.github.com>
2023-09-05 15:30:37 +0700

    feat (documents): direct document download

--- src/components/Profiles/Documents/DocumentsDetailTable.vue
+++ src/components/Profiles/Documents/DocumentsDetailTable.vue
@@ -18,17 +18,23 @@
         <td class="min-w-[228px] font-lato text-blue-gray-500 font-bold text-sm">
           Arsip Dokumen
         </td>
-        <td class="w-full font-lato text-blue-gray-500 font-bold underline text-sm">
+        <td
+          :class="{
+            'w-full font-lato text-blue-gray-500 font-bold  text-sm': true,
+            'underline hover:cursor-pointer': getDocumentName() !== '-',
+          }"
+        >
           <div
             v-if="loading"
             class="h-4 w-1/4 rounded-lg animate-pulse bg-gray-200"
           />
-          <p
+          <a
             v-else
             data-cy="documents-detail__file-name"
+            @click="$emit('download', document.source)"
           >
             {{ getDocumentName() }}
-          </p>
+          </a>
         </td>
       </tr>
       <tr>
